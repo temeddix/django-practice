6292ff3310874b47154f0e151717bd66306d3264
Donghyun Kim <temeddix@gmail.com>
2024-11-29 00:28:30 +0900

    Add related database objects to the admin page

--- polls/admin.py
+++ polls/admin.py
@@ -5,11 +5,17 @@ from django.contrib import admin
 from .models import Question, Choice
 
 
+class ChoiceInline(admin.TabularInline[Choice]):
+    model = Choice
+    extra = 3
+
+
 class QuestionAdmin(admin.ModelAdmin[Question]):
     fieldsets = [
         (None, {"fields": ["question_text"]}),
         ("Date information", {"fields": ["pub_date"]}),
     ]
+    inlines = [ChoiceInline]
 
 
 admin.site.register(Question, QuestionAdmin)
