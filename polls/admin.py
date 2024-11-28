787d0d7e7f953f4ff3e413f0c899705658fdfa44
Donghyun Kim <temeddix@gmail.com>
2024-11-29 00:34:31 +0900

    Customize the admin list page

--- polls/admin.py
+++ polls/admin.py
@@ -16,6 +16,13 @@ class QuestionAdmin(admin.ModelAdmin[Question]):
         ("Date information", {"fields": ["pub_date"]}),
     ]
     inlines = [ChoiceInline]
+    list_display = [
+        "question_text",
+        "pub_date",
+        "was_published_recently",
+    ]
+    list_filter = ["pub_date"]
+    search_fields = ["question_text"]
 
 
 admin.site.register(Question, QuestionAdmin)
