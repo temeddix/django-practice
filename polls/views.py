d4483d619fc4720cd1cfbb58aec1781a9d9df9ae
Donghyun Kim <temeddix@gmail.com>
2024-11-28 22:38:54 +0900

    Rewrite the vote view as a class

--- polls/views.py
+++ polls/views.py
@@ -2,7 +2,7 @@ from django.db.models import F, QuerySet
 from django.http import HttpResponseRedirect, HttpRequest, HttpResponse
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
-from django.views import generic
+from django.views import generic, View
 
 from .models import Choice, Question
 
@@ -26,26 +26,25 @@ class ResultsView(generic.DetailView):
     template_name = "polls/results.html"
 
 
-def vote(request: HttpRequest, question_id: int) -> HttpResponse:
-    question = get_object_or_404(Question, pk=question_id)
-    try:
-        choice_set: QuerySet[Choice] = getattr(question, "choice_set")
-        selected_choice = choice_set.get(pk=request.POST["choice"])
-    except (KeyError, Choice.DoesNotExist):
-        # Redisplay the question voting form.
-        return render(
-            request,
-            "polls/detail.html",
-            {
-                "question": question,
-                "error_message": "You didn't select a choice.",
-            },
-        )
-    else:
-        selected_choice.votes = F("votes") + 1
-        selected_choice.save()
-        # Always return an HttpResponseRedirect after successfully dealing
-        # with POST data. This prevents data from being posted twice if a
-        # user hits the Back button.
-        question_id = getattr(question, "id")
-        return HttpResponseRedirect(reverse("polls:results", args=(question_id,)))
+class VoteView(View):
+    def post(self, request: HttpRequest, question_id: int) -> HttpResponse:
+        question = get_object_or_404(Question, pk=question_id)
+        try:
+            choice_set: QuerySet[Choice] = getattr(question, "choice_set")
+            selected_choice = choice_set.get(pk=request.POST["choice"])
+        except (KeyError, Choice.DoesNotExist):
+            # Redisplay the question voting form with an error message.
+            return render(
+                request,
+                "polls/detail.html",
+                {
+                    "question": question,
+                    "error_message": "You didn't select a choice.",
+                },
+            )
+        else:
+            # Increment the vote count for the selected choice.
+            selected_choice.votes = F("votes") + 1
+            selected_choice.save()
+            # Redirect to the results page after processing the vote.
+            return HttpResponseRedirect(reverse("polls:results", args=(question_id,)))
