17fabefbef80c8d1d3db5e9dbd43c35992afc442
Donghyun Kim <temeddix@gmail.com>
2024-11-30 23:42:13 +0900

    Organize code

--- polls/views.py
+++ polls/views.py
@@ -34,8 +34,8 @@ class ResultsView(generic.DetailView):
 class VoteView(View):
     def post(self, request: HttpRequest, question_id: int) -> HttpResponse:
         question = get_object_or_404(Question, pk=question_id)
+        choice_set: QuerySet[Choice] = getattr(question, "choice_set")
         try:
-            choice_set: QuerySet[Choice] = getattr(question, "choice_set")
             selected_choice = choice_set.get(pk=request.POST["choice"])
         except (KeyError, Choice.DoesNotExist):
             # Redisplay the question voting form with an error message.
